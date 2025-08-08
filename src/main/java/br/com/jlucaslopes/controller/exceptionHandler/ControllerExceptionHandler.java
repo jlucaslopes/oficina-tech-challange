090029005b141f13db464eb5b137fe122d5e92ce
jlucaslopes <jlucasslhp@gmail.com>
2025-08-07 21:28:47 -0300

    adiciona documentacao

--- src/main/java/br/com/jlucaslopes/controller/exceptionHandler/ControllerExceptionHandler.java
+++ src/main/java/br/com/jlucaslopes/controller/exceptionHandler/ControllerExceptionHandler.java
@@ -1,10 +1,9 @@
-package br.com.jlucaslopes.controller;
+package br.com.jlucaslopes.controller.exceptionHandler;
 
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.OffsetDateTime;
